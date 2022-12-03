ce0da53606a7b63671e6e673d7f5711d383c52e6
mrduman <omer.duman@gmx.de>
2022-12-03 17:08:10 +0100

    button and styling

--- componenst/events/event-item.jsx
+++ componenst/events/event-item.jsx
@@ -1,5 +1,11 @@
 import Link from "next/link"
 import classes from "./event-item.module.css"
+import Button from "../ui/button.jsx"
+import AddressIcon from "../icons/address-icon"
+import ArrowRightIcon from "../icons/arrow-right-icon"
+import DateIcon from "../icons/date-icon"
+
+
 
 export default function EventItem(props) {
 
@@ -22,16 +28,21 @@ export default function EventItem(props) {
                         {title}
                     </h2>
                     <time className={classes.date}>
+                        <DateIcon />
                         {humanReadableDate}
                     </time>
                     <div className={classes.address}>
+                        <div> <AddressIcon /></div>
                         <address>
                             {formattedAddress}
                         </address>
                     </div>
                 </div>
                 <div className={classes.actions}>
-                    <Link href={exploreLink}> Explore Event</Link>
+                    <Button link={exploreLink}>
+                        <span>Explore Evenet</span>
+                        <span className={classes.icon}> <ArrowRightIcon /> </span>
+                    </Button>
                 </div>
             </div>
         </li>
